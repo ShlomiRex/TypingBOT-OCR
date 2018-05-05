c620c89c4d9196d8318640e31e66ead2bee2cb16
Shlomi <vgtvgy1@gmail.com>
2018-05-05 17:23:16 +0300

    Working

--- main.py
+++ main.py
@@ -2,28 +2,39 @@ import os
 import pyautogui
 import ConfigParser
 import PIL
+import time
+import pytesseract
 
-#Configuration
-configParser = ConfigParser.RawConfigParser()   
-configFilePath = r'settings.config'
-configParser.read(configFilePath)
-start_x = configParser.get("ScreenshotBounds","start_x")
-start_y = configParser.get("ScreenshotBounds","start_y")
-end_x = configParser.get("ScreenshotBounds","end_x")
-end_y = configParser.get("ScreenshotBounds","end_y")
+#Gives you time to set screen
+time.sleep(3)
 
-print(start_x, start_y, end_x, end_y)
+for x in range(1,30):
 
-#Constants
-image_name = "screenshot.png"
-text_of_image_file_name = "imagetext"
+    #Configuration
+    configParser = ConfigParser.RawConfigParser()   
+    configFilePath = r'settings.config'
+    configParser.read(configFilePath)
+    start_x = configParser.get("ScreenshotBounds","start_x")
+    start_y = configParser.get("ScreenshotBounds","start_y")
+    end_x = configParser.get("ScreenshotBounds","end_x")
+    end_y = configParser.get("ScreenshotBounds","end_y")
 
-#Take screenshot and save
-image = pyautogui.screenshot().save(image_name)
+    print(start_x, start_y, end_x, end_y)
 
-#Crop image to fit bounds of text
-image2 = PIL.Image.open(image_name)
-image2 = image2.crop( ((int)(start_x), (int)(start_y), (int)(end_x), (int)(end_y)) )
-image2.save(image_name)
+    #Constants
+    image_name = "screenshot.png"
+    text_of_image_file_name = "imagetext"
 
-os.system("tesseract " + image_name + " " + text_of_image_file_name)
+    #Take screenshot and save
+    image = pyautogui.screenshot().save(image_name)
+    #Crop image to fit bounds of text
+    image2 = PIL.Image.open(image_name)
+    image2 = image2.crop( ((int)(start_x), (int)(start_y), (int)(end_x), (int)(end_y)) )
+    image2.save(image_name)
+    os.system("tesseract " + image_name + " " + text_of_image_file_name + " -psm 7")
+    text_file = open("imagetext.txt","r")
+
+    text = text_file.read()
+    pyautogui.typewrite(text)
+    pyautogui.typewrite(' ')
+    
