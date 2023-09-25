87036976effe3559fc92663c0133db9b7ec40530
Shlomi <vgtvgy1@gmail.com>
2023-09-25 18:58:04 +0300

    tons of stuff changes, now working

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
+# TypingBot-OCR
+
+This is a bot that takes screenshot of region, reads the text in the image and types it in the browser.
+
+![](README-assets/animation.gif)
+
+![](README-assets/Screenshot%202023-09-25%20at%2018.44.15.png)
+
 Link for website: https://10fastfingers.com/typing-test/english
+
+## Install tesseract
+
+macOS: `brew install tesseract`
+
+Linux: `sudo apt-get install tesseract-ocr`
+
+Windows: download setup / installer at: https://github.com/UB-Mannheim/tesseract/wiki
+
+Make sure you can run it from command line: `tesseract`, if not, add the executable to PATH.
--- main.py
+++ main.py
@@ -1,40 +1,59 @@
 import os
 import pyautogui
-import ConfigParser
+import configparser as ConfigParser
 import PIL
 import time
-import pytesseract
 
-#Gives you time to set screen
-time.sleep(3)
+running = True
 
-for x in range(1,30):
+def run_once():
+    global running
 
+    #Take screenshot and save
+    image_name = "screenshot.png"
+    region_image_name = "region.png"
+    image = pyautogui.screenshot(image_name)
+    image.save(image_name)
+    
+    # Crop
+    region_imgage = PIL.Image.open(image_name)
+    region_imgage = region_imgage.crop((int(start_x), int(start_y), int(end_x), int(end_y)))
+    region_imgage.save(region_image_name)
+
+    #Crop image to fit bounds of text
+    text_of_image_file_name = "imagetext" # Tesseract automatically adds .txt
+    open(text_of_image_file_name+".txt","w+") # Create file
+    os.system("tesseract " + region_image_name + " " + text_of_image_file_name)
+    text_file = open(text_of_image_file_name+".txt","r")
+
+    # Read text from tessaract
+    text = text_file.read()
+    text = text.replace("\n"," ").rstrip().lstrip().replace("  ", " ")
+    text += " " # Last word doesn't have a space after it, we need to go to next image
+
+    # Sometimes we finish fast and so the website will redirect to winner page
+    # Need to stop program from writing to input box
+    if len(text.split()) < 10:
+        running = False
+        return
+
+    print(text)
+    pyautogui.typewrite(text, interval=0, _pause=False)
+
+
+if __name__ == "__main__":
     #Configuration
     configParser = ConfigParser.RawConfigParser()   
-    configFilePath = r'settings.config'
-    configParser.read(configFilePath)
+    configParser.read("settings.config")
     start_x = configParser.get("ScreenshotBounds","start_x")
     start_y = configParser.get("ScreenshotBounds","start_y")
     end_x = configParser.get("ScreenshotBounds","end_x")
     end_y = configParser.get("ScreenshotBounds","end_y")
 
-    print(start_x, start_y, end_x, end_y)
+    #Gives you time to set screen and go to the website
+    time.sleep(5)
 
-    #Constants
-    image_name = "screenshot.png"
-    text_of_image_file_name = "imagetext"
-
-    #Take screenshot and save
-    image = pyautogui.screenshot().save(image_name)
-    #Crop image to fit bounds of text
-    image2 = PIL.Image.open(image_name)
-    image2 = image2.crop( ((int)(start_x), (int)(start_y), (int)(end_x), (int)(end_y)) )
-    image2.save(image_name)
-    os.system("tesseract " + image_name + " " + text_of_image_file_name + " -psm 7")
-    text_file = open("imagetext.txt","r")
-
-    text = text_file.read()
-    pyautogui.typewrite(text)
-    pyautogui.typewrite(' ')
-    
+    for _ in range(100):
+        if not running:
+            break
+        run_once()
